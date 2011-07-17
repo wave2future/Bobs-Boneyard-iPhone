b7f2c5e2b5db0a8c0440c5344edcc64adf3a6f24
Ben <ben.schoenfeld@gmail.com>
2011-07-17 10:56:05 -0400

    Added feed parsing. Still have to work out some issues and then actually do something with the data.

--- BobsBoneyard/EpisodesViewController.h
+++ BobsBoneyard/EpisodesViewController.h
@@ -7,9 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "MWFeedParser.h"
 
 
-@interface EpisodesViewController : UITableViewController {
+@interface EpisodesViewController : UITableViewController <MWFeedParserDelegate> {
     
 }
 
--- BobsBoneyard/EpisodesViewController.m
+++ BobsBoneyard/EpisodesViewController.m
@@ -39,6 +39,22 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    // Create feed parser and pass the URL of the feed
+    NSURL *feedURL = [NSURL URLWithString:@"http://bobsboneyard.com/podcast/boneyard.xml"];
+    MWFeedParser *feedParser = [[MWFeedParser alloc] initWithFeedURL:feedURL];
+    
+    // Delegate must conform to `MWFeedParserDelegate`
+    feedParser.delegate = self;
+    
+    // Parse the feeds info (title, link) and all feed items
+    feedParser.feedParseType = ParseTypeFull;
+    
+    // Connection type
+    feedParser.connectionType = ConnectionTypeAsynchronously;
+    
+    // Begin parsing
+    [feedParser parse];
 }
 
 - (void)viewDidUnload
@@ -52,6 +68,40 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)feedParserDidStart:(MWFeedParser *)parser {
+	NSLog(@"Started Parsing: %@", parser.url);
+}
+
+- (void)feedParser:(MWFeedParser *)parser didParseFeedInfo:(MWFeedInfo *)info {
+	NSLog(@"Parsed Feed Info: “%@”", info.title);
+	self.title = info.title;
+}
+
+- (void)feedParser:(MWFeedParser *)parser didParseFeedItem:(MWFeedItem *)item {
+	NSLog(@"Parsed Feed Item: “%@”", item.title);
+	//if (item) [parsedItems addObject:item];	
+}
+
+- (void)feedParserDidFinish:(MWFeedParser *)parser {
+	NSLog(@"Finished Parsing%@", (parser.stopped ? @" (Stopped)" : @""));
+//	self.itemsToDisplay = [parsedItems sortedArrayUsingDescriptors:
+//						   [NSArray arrayWithObject:[[[NSSortDescriptor alloc] initWithKey:@"date" 
+//																				 ascending:NO] autorelease]]];
+	self.tableView.userInteractionEnabled = YES;
+	self.tableView.alpha = 1;
+	[self.tableView reloadData];
+}
+
+- (void)feedParser:(MWFeedParser *)parser didFailWithError:(NSError *)error {
+	NSLog(@"Finished Parsing With Error: %@", error);
+	self.title = @"Failed";
+//	self.itemsToDisplay = [NSArray array];
+//	[parsedItems removeAllObjects];
+	self.tableView.userInteractionEnabled = YES;
+	self.tableView.alpha = 1;
+	[self.tableView reloadData];
 }
 
 @end
