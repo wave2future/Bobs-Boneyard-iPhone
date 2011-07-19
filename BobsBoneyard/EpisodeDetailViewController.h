063f1b762ef42e53ea433a6cf5945a1670f758a8
Ben <ben.schoenfeld@gmail.com>
2011-07-19 17:44:05 -0400

    Added episode detail view.

---
+++ BobsBoneyard/EpisodeDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  EpisodeDetailViewController.h
+//  BobsBoneyard
+//
+//  Created by Ben on 7/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface EpisodeDetailViewController : UIViewController {
+    
+}
+
+@end
